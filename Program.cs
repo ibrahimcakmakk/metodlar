c55f27d77128fd798474effe9b8c646beb9fb512
ibrahimcakmakk <ibrahim.cakmak@stu.fsm.edu.tr>
2022-08-19 17:21:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args){
+
+        //erişim_belirleyici geri_dönüştipi metod_adi(parametreListesi/argüman)
+            //komutlar
+            //return
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+            int sonuc = Topla(a,b);
+            Metodlar örnek = new Metodlar();
+            örnek.EkranaYazdır(Convert.ToString(sonuc));
+
+            örnek.EkranaYazdır(Convert.ToString(a+b));
+            int sonuc2 = örnek.ArttırVeToplam(a,b);
+            
+    }
+    static int Topla(int deger1,int deger2){
+        return (deger1 + deger2);
+    }
+}
+
+class Metodlar{
+
+    public void EkranaYazdır(String veri){
+        Console.WriteLine(veri);
+    }
+
+    public int ArttırVeToplam(int deger1 , int deger2){
+        deger1 +=1;
+        deger2 +=1;
+        return deger1 + deger2;
+    }
+}
